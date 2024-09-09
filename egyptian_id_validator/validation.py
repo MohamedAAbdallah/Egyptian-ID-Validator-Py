b4509e7779d8f292e6244097abda55443d43cdc0
Mohamed A. Abdallah <eng.mohamed.a.abdallah@gmail.com>
2024-09-09 07:42:23 +0300

    Rewrote the governorate validation

--- egyptian_id_validator/validation.py
+++ egyptian_id_validator/validation.py
@@ -28,7 +28,7 @@ def validate_egyptian_id(national_id: str):
         r'(?P<year>\d{2})'  # 2nd and 3rd digits: Year
         r'(?P<month>0[1-9]|1[0-2])'  # 4th and 5th digits: Month (01-12)
         r'(?P<day>0[1-9]|[12]\d|3[01])'  # 6th and 7th digits: Day (01-31)
-        r'(?P<governorate>0[1-9]|1\d|2[0-9])'  # 8th and 9th digits: Governorate code
+        r'(?P<governorate>0[1-9]|[1-2-3]\d|88)'  # 8th and 9th digits: Governorate code
         r'(?P<unique_number>[0-9]{3})'  # 10th to 12th digits: Unique serial number
         r'(?P<gender>[1-9])'  # 13th digit: Gender code
         r'(\d)$',  # 14th digit: Checksum
@@ -46,15 +46,16 @@ def validate_egyptian_id(national_id: str):
     # Determine the gender from the gender code
     gender = "M" if int(components['gender']) % 2 else "F"
 
-    # Map the governorate code to the governorate name
-    birth_governorate = {
-        1: "Cairo", 2: "Alexandria", 3: "Port Said", 4: "Suez", 11: "Damietta", 12: "Dakahlia", 13: "Sharqia",
-        14: "Qalyubia", 15: "Kafr El Sheikh", 16: "Gharbia", 17: "Monufia", 18: "Beheira", 19: "Ismailia", 21: "Giza",
-        22: "Beni Suef", 23: "Faiyum", 24: "Minya", 25: "Asyut", 26: "Sohag", 27: "Qena", 28: "Aswan", 29: "Luxor",
-        31: "Red Sea", 32: "New Valley", 33: "Matruh", 34: "North Sinai", 35: "South Sinai", 88: "Foreigners"
-    }.get(int(components['governorate']), 'Unknown')
-
-    # Return the extracted information as a dictionary
+    try:
+        birth_governorate = {
+            1: "Cairo", 2: "Alexandria", 3: "Port Said", 4: "Suez", 11: "Damietta", 12: "Dakahlia", 13: "Sharqia",
+            14: "Qalyubia", 15: "Kafr El Sheikh", 16: "Gharbia", 17: "Monufia", 18: "Beheira", 19: "Ismailia", 21: "Giza",
+            22: "Beni Suef", 23: "Faiyum", 24: "Minya", 25: "Asyut", 26: "Sohag", 27: "Qena", 28: "Aswan", 29: "Luxor",
+            31: "Red Sea", 32: "New Valley", 33: "Matruh", 34: "North Sinai", 35: "South Sinai", 88: "Foreigners"
+        }[int(components['governorate'])]
+    except KeyError:
+        return None
+    
     return {
         "Year": year,
         "Month": components['month'],
